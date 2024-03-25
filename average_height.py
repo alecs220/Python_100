cb6a1caac564b78140acafe4b954b997fc4a7ee4
alecsmoto <alexandru.motoonu@gmail.com>
2024-03-25 18:06:05 +0200

    updated_file

--- average_height.py
+++ average_height.py
@@ -1,10 +1,8 @@
-# Input a Python list of student heights
+
 student_heights = input("Enter heights split by space:\n").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
 
-# Write your code below this row 👇
 total_height = 0
 for height in student_heights:
     total_height += height
